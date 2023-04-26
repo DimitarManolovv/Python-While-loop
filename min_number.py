419875f1000b5c2820fc6fecbf170ec17ae62199
DimitarManolovv <108031142+DimitarManolovv@users.noreply.github.com>
2023-04-26 23:19:29 +0300

    Add files via upload

---
+++ min_number.py
@@ -0,0 +1,10 @@
+number = input()
+min_number = float('inf')
+
+while number != "Stop":
+    current_number = int(number)
+    if current_number < min_number:
+        min_number = current_number
+    number = input()
+
+print(min_number)
